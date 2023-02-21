91e915231962673321f946f9b75dcca2431a7be2
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-02-21 15:59:23 +0100

    add system mappings

--- src/mappings/system.ts
+++ src/mappings/system.ts
@@ -16,42 +16,59 @@ export function normalizeSystemArgs(ctx: ChainContext, event: Event) {
     case "System.CodeUpdated":
       e = new SystemCodeUpdatedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        return null;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.ExtrinsicFailed":
       e = new SystemExtrinsicFailedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [error, info] = e.asV5;
+        return {
+          error,
+          info,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.ExtrinsicSuccess":
       e = new SystemExtrinsicSuccessEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let info = e.asV5;
+        return {
+          info,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.KilledAccount":
       e = new SystemKilledAccountEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let account = e.asV5;
+        return {
+          account: bufferToHex(account),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.NewAccount":
       e = new SystemNewAccountEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let account = e.asV5;
+        return {
+          account: bufferToHex(account),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.Remarked":
       e = new SystemRemarkedEvent(ctx, event);
       if (e.isV8) {
-        // YOUR CODE HERE
+        let [origin, remark_hash] = e.asV8;
+        return {
+          origin: bufferToHex(origin),
+          remark_hash: bufferToHex(remark_hash),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
