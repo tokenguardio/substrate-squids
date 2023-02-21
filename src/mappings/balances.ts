687315b41327671fd649d3dfac434afd76676696
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-02-21 15:10:05 +0100

    add Balances mappings

--- src/mappings/balances.ts
+++ src/mappings/balances.ts
@@ -31,48 +31,79 @@ export function normalizeEventArgs(ctx: ChainContext, event: Event) {
       e = new BalancesDepositEvent(ctx, event);
       if (e.isV5) {
         let [who, deposit] = e.asV5;
+        return {
+          who: bufferToHex(who),
+          deposit,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.DustLost":
       e = new BalancesDustLostEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [account, balance] = e.asV5;
+        return {
+          account: bufferToHex(account),
+          balance,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Endowed":
       e = new BalancesEndowedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [account, free_balance] = e.asV5;
+        return {
+          account: bufferToHex(account),
+          free_balance,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.ReserveRepatriated":
       e = new BalancesReserveRepatriatedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [from, to, balance, destination_status] = e.asV5;
+        return {
+          from: bufferToHex(from),
+          to: bufferToHex(to),
+          balance,
+          destination_status,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Reserved":
       e = new BalancesReservedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [who, value] = e.asV5;
+        return {
+          who: bufferToHex(who),
+          value,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Transfer":
       e = new BalancesTransferEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [from, to, value] = e.asV5;
+        return {
+          from: bufferToHex(from),
+          to: bufferToHex(to),
+          value,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Unreserved":
       e = new BalancesUnreservedEvent(ctx, event);
       if (e.isV5) {
-        // YOUR CODE HERE
+        let [who, value] = e.asV5;
+        return {
+          who: bufferToHex(who),
+          value,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
