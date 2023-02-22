96863885534bffcc64d50d6c4489c3fbad4c1654
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-02-22 13:37:10 +0100

    update processor

--- src/processor.ts
+++ src/processor.ts
@@ -1,28 +1,16 @@
-import { lookupArchive } from "@subsquid/archive-registry";
-import * as ss58 from "@subsquid/ss58";
+import { SubstrateBatchProcessor } from "@subsquid/substrate-processor";
+import { TypeormDatabase } from "@subsquid/typeorm-store";
+import { EventNorm } from "./model";
 import {
-  BatchContext,
-  BatchProcessorItem,
-  SubstrateBatchProcessor,
-} from "@subsquid/substrate-processor";
-import { Store, TypeormDatabase } from "@subsquid/typeorm-store";
-import {
-  Event,
-  Block,
-  Call,
-  Extrinsic,
-  Metadata,
-  Address,
-  BalancesPallet,
-  StakingPallet,
-  NominationPoolsPallet,
-} from "./model";
-import { MetadataResponse } from "./interfaces/gql-interfaces";
-import {
-  GraphqlRequest,
-  graphqlRequest,
-} from "@subsquid/util-internal-gql-request";
-import { normalizeEventArgs } from "./mappings/events";
+  normalizeBalancesEventArgs,
+  normalizeStakingEventArgs,
+  normalizeSystemEventArgs,
+} from "./mappings";
+
+//@ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 
 const processor = new SubstrateBatchProcessor()
   .setDataSource({
@@ -32,181 +20,45 @@ const processor = new SubstrateBatchProcessor()
     data: {
       event: true,
     },
-  })
-  .addCall("*", {
-    data: {
-      call: true,
-      extrinsic: true,
-    },
   } as const);
 
-type Item = BatchProcessorItem<typeof processor>;
-type Ctx = BatchContext<Store, Item>;
-
-let events: Event[] = [];
-let extrinsics: Extrinsic[] = [];
-let calls: Call[] = [];
-let metadata: Metadata[] = [];
-let blocks: Block[] = [];
-let addresses: Address[] = [];
-let balancesPallet: BalancesPallet[] = [];
-let stakingPallet: StakingPallet[] = [];
-let nominationPoolsPallet: NominationPoolsPallet[] = [];
-
-const metadataRequest: GraphqlRequest = {
-  url: `https://reef.archive.subsquid.io/graphql`,
-  query:
-    "query MyQuery { metadata { blockHash blockHeight id specName specVersion hex}}",
-  retry: true,
-};
-let metadataInitialized = false;
+let events: EventNorm[] = [];
 
 processor.run(new TypeormDatabase(), async (ctx) => {
-  if (!metadataInitialized) {
-    ctx.log.info("Initizalizing metadata");
-    const metadataResponse: MetadataResponse = await graphqlRequest(
-      metadataRequest
-    );
-    metadata = metadataResponse["metadata"].map(
-      (m) =>
-        new Metadata({
-          id: m.id,
-          specName: m.specName,
-          specVersion: m.specVersion,
-          blockHeight: m.blockHeight,
-          blockHash: m.blockHash,
-          hex: m.hex,
-        })
-    );
-    await ctx.store.save(metadata);
-    metadataInitialized = true;
-  }
-
   for (let block of ctx.blocks) {
-    let metadataInstance = metadata.find((m) => m.id === block.header.specId);
-    let blockOrm = new Block({
-      id: block.header.id,
-      height: block.header.height,
-      hash: block.header.hash,
-      parentHash: block.header.parentHash,
-      timestamp: new Date(block.header.timestamp),
-      validator: block.header.validator,
-      stateRoot: block.header.stateRoot,
-      extrinsicsRoot: block.header.extrinsicsRoot,
-      spec: metadataInstance,
-    });
-    blocks.push(blockOrm);
     for (let item of block.items) {
-      if (item.kind === "event") {
-        // ctx.log.info(item);
-        let extrinsic = undefined;
-        if (item.event.extrinsic) {
-          extrinsic = new Extrinsic({
-            id: item.event.extrinsic?.id,
-            block: blockOrm,
-            indexInBlock: item.event.extrinsic?.indexInBlock,
-            version: item.event.extrinsic?.version,
-            signature: item.event.extrinsic?.signature,
-            success: item.event.extrinsic?.success,
-            error: item.event.extrinsic?.error,
-            fee: item.event.extrinsic?.fee,
-            tip: item.event.extrinsic?.tip,
-            hash: item.event.extrinsic?.hash,
-            pos: item.event.extrinsic?.pos,
-          });
-          extrinsics.push(extrinsic);
+      // Check if the item is an event and if its name starts with one of the prefixes
+      if (
+        item.kind === "event" &&
+        (item.event.name.startsWith("Balances.") ||
+          item.event.name.startsWith("Staking.") ||
+          item.event.name.startsWith("System."))
+      ) {
+        // Normalize the event arguments based on the prefix
+        let args;
+        switch (true) {
+          case item.event.name.startsWith("Balances."):
+            args = normalizeBalancesEventArgs(ctx, item.event);
+            break;
+          case item.event.name.startsWith("Staking."):
+            args = normalizeStakingEventArgs(ctx, item.event);
+            break;
+          case item.event.name.startsWith("System."):
+            args = normalizeSystemEventArgs(ctx, item.event);
+            break;
         }
-        let call = undefined;
-        if (item.event.call) {
-          call = new Call({
-            id: item.event.call?.id,
-            block: blockOrm,
-            extrinsic: extrinsic,
-            success: item.event.call?.success,
-            error: item.event.call?.error,
-            origin: item.event.call?.origin,
-            name: item.event.call?.name,
-            args: item.event.call?.args,
-            pos: item.event.call?.pos,
-          });
-          calls.push(call);
-        }
-        let args = normalizeEventArgs(ctx, item.event);
-        let event = new Event({
+
+        // Create a new event object and push it to the events array
+        const event = new EventNorm({
           id: item.event.id,
-          block: blockOrm,
-          indexInBlock: item.event.indexInBlock,
-          phase: item.event.phase,
-          extrinsic: extrinsic,
-          call: call,
+          blockHash: block.header.hash,
+          timestamp: new Date(block.header.timestamp),
           name: item.event.name,
-          args: args,
-          pos: item.event.pos,
+          args,
         });
         events.push(event);
-      } else if (item.kind === "call") {
-        // ctx.log.info(item);
-        let extrinsic = new Extrinsic({
-          id: item.extrinsic?.id,
-          block: blockOrm,
-          indexInBlock: item.extrinsic?.indexInBlock,
-          version: item.extrinsic?.version,
-          signature: item.extrinsic?.signature,
-          success: item.extrinsic?.success,
-          error: item.extrinsic?.error,
-          fee: item.extrinsic?.fee,
-          tip: item.extrinsic?.tip,
-          hash: item.extrinsic?.hash,
-          pos: item.extrinsic?.pos,
-        });
-        extrinsics.push(extrinsic);
-        let call = new Call({
-          id: item.call.id,
-          block: blockOrm,
-          extrinsic: extrinsic,
-          success: item.call.success,
-          error: item.call.error,
-          origin: item.call.origin,
-          name: item.call.name,
-          args: item.call.args,
-          pos: item.call.pos,
-        });
-        calls.push(call);
       }
     }
+    await ctx.store.save(events);
   }
-
-  await ctx.store.save(removeDuplicates(blocks, "id"));
-  await ctx.store.save(removeDuplicates(extrinsics, "id"));
-  await ctx.store.save(removeDuplicates(calls, "id"));
-  await ctx.store.save(removeDuplicates(events, "id"));
-  await ctx.store.save(removeDuplicates(addresses, "id"));
-  await ctx.store.save(removeDuplicates(balancesPallet, "id"));
-  await ctx.store.save(removeDuplicates(stakingPallet, "id"));
-  await ctx.store.save(removeDuplicates(nominationPoolsPallet, "id"));
 });
-
-function createAccount(
-  hexAddress: string | undefined,
-  addresses: Address[]
-): Address | null {
-  if (hexAddress == null) {
-    return null;
-  }
-  const address = new Address({
-    id: hexAddress,
-    ss58: ss58.codec(42).encode(Buffer.from(hexAddress.slice(2), "hex")),
-  });
-  addresses.push(address);
-  return address;
-}
-
-function removeDuplicates<T>(items: T[], key: keyof T): T[] {
-  const seen = new Set();
-  const filtered = items.filter((i) => {
-    const duplicate = seen.has(i[key]);
-    seen.add(i[key]);
-    return !duplicate;
-  });
-  return filtered;
-}
