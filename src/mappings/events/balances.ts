c59ffb5bf5a686112200b527e3541f5eb9b41a1c
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-01 22:11:24 +0100

    add balances mappings

--- src/mappings/events/balances.ts
+++ src/mappings/events/balances.ts
@@ -20,70 +20,70 @@ export function normalizeBalancesEventsArgs(ctx: ChainContext, event: Event) {
     case "Balances.BalanceSet":
       e = new BalancesBalanceSetEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Deposit":
       e = new BalancesDepositEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.DustLost":
       e = new BalancesDustLostEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Endowed":
       e = new BalancesEndowedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.ReserveRepatriated":
       e = new BalancesReserveRepatriatedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Reserved":
       e = new BalancesReservedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Slashed":
       e = new BalancesSlashedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Transfer":
       e = new BalancesTransferEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Unreserved":
       e = new BalancesUnreservedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Balances.Withdraw":
       e = new BalancesWithdrawEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
