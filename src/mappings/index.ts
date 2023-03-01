cd725aabf3c469546aec8e3cb8fb559d93d6f109
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-01 23:31:56 +0100

    update processor and db

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,3 +1,6 @@
 export * from "./events/system";
 export * from "./events/balances";
 export * from "./events/staking";
+export * from "./events/contracts";
+
+export * from "./calls/contracts";
--- src/processor.ts
+++ src/processor.ts
@@ -1,10 +1,12 @@
 import { SubstrateBatchProcessor } from "@subsquid/substrate-processor";
 import { TypeormDatabase } from "@subsquid/typeorm-store";
-import { EventNorm } from "./model";
+import { EventNorm, CallNorm } from "./model";
 import {
   normalizeBalancesEventsArgs,
   normalizeStakingEventsArgs,
   normalizeSystemEventsArgs,
+  normalizeContractsEventsArgs,
+  normalizeContractsCallsArgs,
 } from "./mappings";
 
 // Avoid type errors when serializing BigInts
@@ -14,16 +16,25 @@ import {
 
 const processor = new SubstrateBatchProcessor()
   .setDataSource({
-    archive: `https://reef.archive.subsquid.io/graphql`,
+    archive: `https://aleph-zero-testnet.archive.subsquid.io/graphql`,
   })
   .addEvent("*", {
     data: {
       event: true,
     },
-  } as const);
+  })
+  .addCall("*", {
+    data: {
+      call: true,
+      extrinsic: {
+        success: true,
+      },
+    },
+  });
 
 processor.run(new TypeormDatabase(), async (ctx) => {
   let events: EventNorm[] = [];
+  let calls: CallNorm[] = [];
   for (let block of ctx.blocks) {
     for (let item of block.items) {
       // Check if the item is an event and if its name starts with one of the prefixes
@@ -31,7 +42,8 @@ processor.run(new TypeormDatabase(), async (ctx) => {
         item.kind === "event" &&
         (item.event.name.startsWith("Balances.") ||
           item.event.name.startsWith("Staking.") ||
-          item.event.name.startsWith("System.")) &&
+          item.event.name.startsWith("System.") ||
+          item.event.name.startsWith("Contracts.")) &&
         !["System.ExtrinsicSuccess", "System.ExtrinsicFailed"].includes(
           item.event.name
         )
@@ -48,6 +60,9 @@ processor.run(new TypeormDatabase(), async (ctx) => {
           case item.event.name.startsWith("System."):
             args = normalizeSystemEventsArgs(ctx, item.event);
             break;
+          case item.event.name.startsWith("Contracts."):
+            args = normalizeContractsEventsArgs(ctx, item.event);
+            break;
         }
 
         // Create a new event object and push it to the events array
@@ -60,8 +75,31 @@ processor.run(new TypeormDatabase(), async (ctx) => {
           extrinsicSuccess: item.event.extrinsic?.success,
         });
         events.push(event);
+      } else if (
+        item.kind === "call" &&
+        item.call.name.startsWith("Contracts.")
+      ) {
+        // Normalize the call arguments based on the prefix
+        let args;
+        switch (true) {
+          case item.call.name.startsWith("Contracts."):
+            args = normalizeContractsCallsArgs(ctx, item.call);
+            break;
+        }
+
+        // Create a new call object and push it to the calls array
+        const call = new CallNorm({
+          id: item.call.id,
+          blockHash: block.header.hash,
+          timestamp: new Date(block.header.timestamp),
+          name: item.call.name,
+          args,
+          success: item.call.success,
+        });
+        calls.push(call);
       }
     }
   }
+  await ctx.store.save(calls);
   await ctx.store.save(events);
 });
