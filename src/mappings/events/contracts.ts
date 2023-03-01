c0fdc6741939d0330d4c4ab4eb1a68a3eb339fd7
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-01 22:03:51 +0100

    add contracts mappings

--- src/mappings/events/contracts.ts
+++ src/mappings/events/contracts.ts
@@ -18,56 +18,56 @@ export function normalizeContractsEventsArgs(ctx: ChainContext, event: Event) {
     case "Contracts.Called":
       e = new ContractsCalledEvent(ctx, event);
       if (e.isV45) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.CodeRemoved":
       e = new ContractsCodeRemovedEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.CodeStored":
       e = new ContractsCodeStoredEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.ContractCodeUpdated":
       e = new ContractsContractCodeUpdatedEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.ContractEmitted":
       e = new ContractsContractEmittedEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.DelegateCalled":
       e = new ContractsDelegateCalledEvent(ctx, event);
       if (e.isV45) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.Instantiated":
       e = new ContractsInstantiatedEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Contracts.Terminated":
       e = new ContractsTerminatedEvent(ctx, event);
       if (e.isV20) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
