c1d4b2d7ba5a360a46e48a48e1facf1926e9d798
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-01 19:53:36 +0100

    add staking mappings

--- src/mappings/events/staking.ts
+++ src/mappings/events/staking.ts
@@ -23,91 +23,134 @@ export function normalizeStakingEventsArgs(ctx: ChainContext, event: Event) {
     case "Staking.Bonded":
       e = new StakingBondedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [stash, amount] = e.asV12;
+        return {
+          stash: bufferToHex(stash),
+          amount,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Chilled":
       e = new StakingChilledEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let stash = e.asV12;
+        return {
+          stash: bufferToHex(stash),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.EraPaid":
       e = new StakingEraPaidEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [eraIndex, validatorPayout, remainder] = e.asV12;
+        return {
+          eraIndex,
+          validatorPayout,
+          remainder,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Kicked":
       e = new StakingKickedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [nominator, stash] = e.asV12;
+        return {
+          nominator: bufferToHex(nominator),
+          stash: bufferToHex(stash),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.OldSlashingReportDiscarded":
       e = new StakingOldSlashingReportDiscardedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let sessionIndex = e.asV12;
+        return {
+          sessionIndex,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.PayoutStarted":
       e = new StakingPayoutStartedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [eraIndex, validatorStash] = e.asV12;
+        return {
+          eraIndex,
+          validatorStash: bufferToHex(validatorStash),
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Rewarded":
       e = new StakingRewardedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [stash, amount] = e.asV12;
+        return {
+          stash: bufferToHex(stash),
+          amount,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Slashed":
       e = new StakingSlashedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [validator, amount] = e.asV12;
+        return {
+          validator: bufferToHex(validator),
+          amount,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.StakersElected":
       e = new StakingStakersElectedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.StakingElectionFailed":
       e = new StakingStakingElectionFailedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Unbonded":
       e = new StakingUnbondedEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [stash, amount] = e.asV12;
+        return {
+          stash: bufferToHex(stash),
+          amount,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.ValidatorPrefsSet":
       e = new StakingValidatorPrefsSetEvent(ctx, event);
       if (e.isV39) {
-        // YOUR CODE HERE
+        let [stash, prefs] = e.asV39;
+        return {
+          stash: bufferToHex(stash),
+          prefs,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "Staking.Withdrawn":
       e = new StakingWithdrawnEvent(ctx, event);
       if (e.isV12) {
-        // YOUR CODE HERE
+        let [stash, amount] = e.asV12;
+        return {
+          stash: bufferToHex(stash),
+          amount,
+        };
       } else {
         throw new UnknownVersionError(event.name);
       }
