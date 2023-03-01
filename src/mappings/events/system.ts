27ee316c62e70b59d500906d7d9c41aa6255dfd3
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-01 19:21:49 +0100

    add system mappings

--- src/mappings/events/system.ts
+++ src/mappings/events/system.ts
@@ -10,65 +10,79 @@ import { ChainContext, Event } from "../../types/support";
 import { bufferToHex } from "../../utils/utils";
 import { UnknownVersionError, UnknownEventError } from "../../utils/errors";
 
-export function normalizeSystemEventArgs(ctx: ChainContext, event: Event) {
+export function normalizeSystemEventsArgs(ctx: ChainContext, event: Event) {
   let e;
   switch (event.name) {
     case "System.CodeUpdated":
       e = new SystemCodeUpdatedEvent(ctx, event);
-      if (e.isV5) {
-        return null;
+      if (e.isV3) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.ExtrinsicFailed":
       e = new SystemExtrinsicFailedEvent(ctx, event);
-      if (e.isV5) {
-        let [error, info] = e.asV5;
+      if (e.isV3) {
+        let [error, info] = e.asV3;
         return {
-          error,
-          info,
+          dispatchError: error,
+          dispatchInfo: info,
         };
+      } else if (e.isV12) {
+        let [error, info] = e.asV12;
+        return {
+          dispatchError: error,
+          dispatchInfo: info,
+        };
+      } else if (e.isV39) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.ExtrinsicSuccess":
       e = new SystemExtrinsicSuccessEvent(ctx, event);
-      if (e.isV5) {
-        let info = e.asV5;
-        return {
-          info,
-        };
+      if (e.isV3) {
+        let dispatchInfo = e.asV3;
+        return { dispatchInfo };
+      } else if (e.isV39) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.KilledAccount":
       e = new SystemKilledAccountEvent(ctx, event);
-      if (e.isV5) {
-        let account = e.asV5;
+      if (e.isV3) {
+        let account = e.asV3;
         return {
           account: bufferToHex(account),
         };
+      } else if (e.isV39) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.NewAccount":
       e = new SystemNewAccountEvent(ctx, event);
-      if (e.isV5) {
-        let account = e.asV5;
+      if (e.isV3) {
+        let account = e.asV3;
         return {
           account: bufferToHex(account),
         };
+      } else if (e.isV39) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
     case "System.Remarked":
       e = new SystemRemarkedEvent(ctx, event);
-      if (e.isV8) {
-        let [origin, remark_hash] = e.asV8;
+      if (e.isV3) {
+        let [origin, remarkHash] = e.asV3;
         return {
-          origin: bufferToHex(origin),
-          remark_hash: bufferToHex(remark_hash),
+          sender: bufferToHex(origin),
+          hash: bufferToHex(remarkHash),
         };
+      } else if (e.isV39) {
+        return event.args;
       } else {
         throw new UnknownVersionError(event.name);
       }
