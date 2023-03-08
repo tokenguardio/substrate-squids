798f50f142fe5e2bfa6ee60f86aaec170712289a
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-08 08:45:15 +0100

    add calls mappings

--- src/mappings/calls/contracts.ts
+++ src/mappings/calls/contracts.ts
@@ -19,63 +19,63 @@ export function normalizeContractsCallsArgs(ctx: ChainContext, call: Call) {
     case "Contracts.call":
       e = new ContractsCallCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.call_old_weight":
       e = new ContractsCallOldWeightCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.instantiate":
       e = new ContractsInstantiateCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.instantiate_old_weight":
       e = new ContractsInstantiateOldWeightCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.instantiate_with_code":
       e = new ContractsInstantiateWithCodeCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.instantiate_with_code_old_weight":
       e = new ContractsInstantiateWithCodeOldWeightCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.remove_code":
       e = new ContractsRemoveCodeCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.set_code":
       e = new ContractsSetCodeCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
     case "Contracts.upload_code":
       e = new ContractsUploadCodeCall(ctx, call);
       if (e.isV3012) {
-        // YOUR CODE HERE
+        return call.args;
       } else {
         throw new UnknownCallVersionError(call.name);
       }
