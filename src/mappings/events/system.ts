fcfb3a61d19931c4147231b8179880d954b09374
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-03-08 08:30:53 +0100

    add system mappings

--- src/mappings/events/system.ts
+++ src/mappings/events/system.ts
@@ -19,62 +19,77 @@ export function normalizeSystemEventsArgs(ctx: ChainContext, event: Event) {
     case "System.CodeUpdated":
       e = new SystemCodeUpdatedEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
     case "System.ExtrinsicFailed":
       e = new SystemExtrinsicFailedEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        let [error, info] = e.asEfinityV1;
+        return {
+          dispatchError: error,
+          dispatchInfo: info,
+        };
       } else if (e.isEfinityV2) {
-        // YOUR CODE HERE
+        return event.args;
       } else if (e.isEfinityV3) {
-        // YOUR CODE HERE
+        return event.args;
       } else if (e.isEfinityV3000) {
-        // YOUR CODE HERE
+        return event.args;
       } else if (e.isEfinityV3012) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
     case "System.ExtrinsicSuccess":
       e = new SystemExtrinsicSuccessEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        let dispatchInfo = e.asEfinityV1;
+        return { dispatchInfo };
       } else if (e.isEfinityV2) {
-        // YOUR CODE HERE
+        return event.args;
       } else if (e.isEfinityV3000) {
-        // YOUR CODE HERE
+        return event.args;
       } else if (e.isEfinityV3012) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
     case "System.KilledAccount":
       e = new SystemKilledAccountEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        let account = e.asEfinityV1;
+        return {
+          account: bufferToHex(account),
+        };
       } else if (e.isEfinityV2) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
     case "System.NewAccount":
       e = new SystemNewAccountEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        let account = e.asEfinityV1;
+        return {
+          account: bufferToHex(account),
+        };
       } else if (e.isEfinityV2) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
     case "System.Remarked":
       e = new SystemRemarkedEvent(ctx, event);
       if (e.isEfinityV1) {
-        // YOUR CODE HERE
+        let [origin, remarkHash] = e.asEfinityV1;
+        return {
+          sender: bufferToHex(origin),
+          hash: bufferToHex(remarkHash),
+        };
       } else if (e.isEfinityV2) {
-        // YOUR CODE HERE
+        return event.args;
       } else {
         throw new UnknownEventVersionError(event.name);
       }
