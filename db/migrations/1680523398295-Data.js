f44408548bc7bb57574d3f5ef7bf88955fd4bd06
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-04-03 14:05:04 +0200

    update migration

--- db/migrations/1680523398295-Data.js
+++ db/migrations/1680523398295-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1677764635842 {
-    name = 'Data1677764635842'
+module.exports = class Data1680523398295 {
+    name = 'Data1680523398295'
 
     async up(db) {
         await db.query(`CREATE TABLE "event_norm" ("id" character varying NOT NULL, "block_hash" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "name" text NOT NULL, "args" jsonb, "extrinsic_success" boolean, CONSTRAINT "PK_d33300243feb4679ec112a74a03" PRIMARY KEY ("id"))`)
@@ -8,6 +8,8 @@ module.exports = class Data1677764635842 {
         await db.query(`CREATE TABLE "call_norm" ("id" character varying NOT NULL, "block_hash" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "name" text NOT NULL, "args" jsonb, "success" boolean, "origin" jsonb, CONSTRAINT "PK_e6dee5a463d3dd8993668fb913e" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_5fc3745dc44160b5563cf88307" ON "call_norm" ("timestamp") `)
         await db.query(`CREATE INDEX "IDX_d069b0512be986815a0a41e601" ON "call_norm" ("name") `)
+        await db.query(`CREATE TABLE "address_mapping" ("id" character varying NOT NULL, "ss58" text NOT NULL, CONSTRAINT "PK_8611a631b9c1187979a08ecb53f" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE INDEX "IDX_3a046999bc4c3e7c38468ddf80" ON "address_mapping" ("ss58") `)
     }
 
     async down(db) {
@@ -17,5 +19,7 @@ module.exports = class Data1677764635842 {
         await db.query(`DROP TABLE "call_norm"`)
         await db.query(`DROP INDEX "public"."IDX_5fc3745dc44160b5563cf88307"`)
         await db.query(`DROP INDEX "public"."IDX_d069b0512be986815a0a41e601"`)
+        await db.query(`DROP TABLE "address_mapping"`)
+        await db.query(`DROP INDEX "public"."IDX_3a046999bc4c3e7c38468ddf80"`)
     }
 }
