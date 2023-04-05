b5b89e3796c921b160a4c0231ca3906e0186ae53
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-04-05 13:02:45 +0200

    change address prefix

--- src/mappings/specific/account.ts
+++ src/mappings/specific/account.ts
@@ -10,13 +10,13 @@ export function mapAccount(ctx: ChainContext, event: Event) {
     let account = e.asV1050;
     return {
       account_hex: bufferToHex(account),
-      account_ss58: ss58.codec("substrate").encode(account),
+      account_ss58: ss58.codec("kusama").encode(account),
     };
   } else if (e.isV9160) {
     let { account } = e.asV9160;
     return {
       account_hex: bufferToHex(account),
-      account_ss58: ss58.codec("substrate").encode(account),
+      account_ss58: ss58.codec("kusama").encode(account),
     };
   } else {
     throw new UnknownEventVersionError(event.name);
