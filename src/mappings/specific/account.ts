714a3eb35de2fb580431cc4ffa74279e7102bbc8
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-04-05 13:47:02 +0200

    update prefix

--- src/mappings/specific/account.ts
+++ src/mappings/specific/account.ts
@@ -10,13 +10,13 @@ export function mapAccount(ctx: ChainContext, event: Event) {
     let account = e.asV23;
     return {
       account_hex: bufferToHex(account),
-      account_ss58: ss58.codec("substrate").encode(account),
+      account_ss58: ss58.codec("zeitgeist").encode(account),
     };
   } else if (e.isV34) {
     let { account } = e.asV34;
     return {
       account_hex: bufferToHex(account),
-      account_ss58: ss58.codec("substrate").encode(account),
+      account_ss58: ss58.codec("zeitgeist").encode(account),
     };
   } else {
     throw new UnknownEventVersionError(event.name);
