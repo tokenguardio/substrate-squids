888f8ccf7dd926bb9ccad0518b81880304baf5c8
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-05-16 17:54:52 +0200

    add address mapping to all events/calls

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,8 +1,5 @@
-export * from "./pallets/events/system";
-export * from "./pallets/events/balances";
-export * from "./pallets/events/staking";
-export * from "./pallets/events/nomination-pools";
+export * from "./pallets/handlers";
 
-export * from "./pallets/calls/nomination-pools";
-
-export * from "./specific/account";
+export * from "./addresses/mapAddresses";
+export * from "./addresses/eventsAddressArgs";
+export * from "./addresses/callsAddressArgs";
--- src/processor.ts
+++ src/processor.ts
@@ -5,19 +5,13 @@ import {
 import { TypeormDatabase } from "@subsquid/typeorm-store";
 import { EventNorm, CallNorm, AddressMapping } from "./model";
 import {
-  normalizeBalancesEventsArgs,
-  normalizeStakingEventsArgs,
-  normalizeSystemEventsArgs,
-  normalizeContractsEventsArgs,
-  normalizeEVMEventsArgs,
-  normalizeEthereumEventsArgs,
-  normalizeEVMCallsArgs,
-  normalizeEthereumCallsArgs,
-  normalizeContractsCallsArgs,
-  mapAddress,
+  eventNormalizationHandlers,
+  callNormalizationHandlers,
+  eventsAddressArgs,
+  callsAddressArgs,
+  mapAddresses,
 } from "./mappings";
-import { MappedAddress } from "./interfaces/mappings/specific";
-import { removeDuplicates } from "./utils/utils";
+import { MappedAddress, AddressArgs } from "./interfaces/mappings/specific";
 
 // Avoid type errors when serializing BigInts
 (BigInt.prototype as any).toJSON = function () {
@@ -26,7 +20,7 @@ import { removeDuplicates } from "./utils/utils";
 
 const processor = new SubstrateBatchProcessor()
   .setDataSource({
-    archive: `${process.env.ARCHIVE_GATEWAY_HOST}:${process.env.ARCHIVE_GATEWAY_PORT}/graphql`,
+    archive: `https://astar.archive.subsquid.io/graphql`,
   })
   .addEvent("*", {
     data: {
@@ -45,62 +39,39 @@ const processor = new SubstrateBatchProcessor()
 processor.run(new TypeormDatabase(), async (ctx) => {
   const events: EventNorm[] = [];
   const calls: CallNorm[] = [];
-  const addressMappings: AddressMapping[] = [];
+  const addressMappings: Map<string, AddressMapping> = new Map();
+
   for (const block of ctx.blocks) {
     for (const item of block.items) {
-      // Check if the item is an event and if its name starts with one of the prefixes
-      if (
-        item.kind === "event" &&
-        (item.event.name.startsWith("Balances.") ||
-          item.event.name.startsWith("Staking.") ||
-          item.event.name.startsWith("System.") ||
-          item.event.name.startsWith("NominationPools.")) &&
-        !["System.ExtrinsicSuccess", "System.ExtrinsicFailed"].includes(
-          item.event.name
-        )
-      ) {
-        // Normalize the event arguments based on the prefix
-        let args;
-        switch (true) {
-          case item.event.name.startsWith("Balances."):
-            args = normalizeBalancesEventsArgs(ctx, item.event);
-            break;
-          case item.event.name.startsWith("Staking."):
-            args = normalizeStakingEventsArgs(ctx, item.event);
-            break;
-          case item.event.name.startsWith("System."):
-            args = normalizeSystemEventsArgs(ctx, item.event);
-            break;
-          case item.event.name.startsWith("NominationPools."):
-            args = normalizeNominationPoolsEventsArgs(ctx, item.event);
-            break;
-        }
-
-  for (let block of ctx.blocks) {
-    for (let item of block.items) {
+      const pallet = item.name.split(".")[0];
       if (item.kind === "event") {
-        const prefix = item.event.name.split(".")[0] + ".";
         if (
-          eventHandlers[prefix] &&
+          eventNormalizationHandlers[pallet] &&
           !["System.ExtrinsicSuccess", "System.ExtrinsicFailed"].includes(
             item.event.name
           )
         ) {
-          let args = eventHandlers[prefix](ctx, item.event);
+          const args = eventNormalizationHandlers[pallet](ctx, item.event);
           const event = createEventNorm(block.header, item.event, args);
           events.push(event);
-        }
-        if ((item.event.name as string) === "System.NewAccount") {
-          const mappedAddress = mapAddress(ctx, item.event);
-          const addressMapping = createAddressMapping(mappedAddress);
-          addressMappings.push(addressMapping);
+          addAddressesToMap(
+            item.event.name,
+            args,
+            eventsAddressArgs,
+            addressMappings
+          );
         }
       } else if (item.kind === "call") {
-        const prefix = item.call.name.split(".")[0] + ".";
-        if (callHandlers[prefix]) {
-          let args = callHandlers[prefix](ctx, item.call);
+        if (callNormalizationHandlers[pallet]) {
+          const args = callNormalizationHandlers[pallet](ctx, item.call);
           const call = createCallNorm(block.header, item.call, args);
           calls.push(call);
+          addAddressesToMap(
+            item.call.name,
+            args,
+            callsAddressArgs,
+            addressMappings
+          );
         }
       }
     }
@@ -108,10 +79,14 @@ processor.run(new TypeormDatabase(), async (ctx) => {
 
   await ctx.store.save(events);
   await ctx.store.save(calls);
-  await ctx.store.save(removeDuplicates(addressMappings, "id"));
+  await ctx.store.save(Array.from(addressMappings.values()));
 });
 
-function createEventNorm(block: SubstrateBlock, event: any, args: any) {
+function createEventNorm(
+  block: SubstrateBlock,
+  event: any,
+  args: any
+): EventNorm {
   return new EventNorm({
     id: event.id,
     blockHash: block.hash,
@@ -122,7 +97,7 @@ function createEventNorm(block: SubstrateBlock, event: any, args: any) {
   });
 }
 
-function createCallNorm(block: SubstrateBlock, call: any, args: any) {
+function createCallNorm(block: SubstrateBlock, call: any, args: any): CallNorm {
   return new CallNorm({
     id: call.id,
     blockHash: block.hash,
@@ -134,9 +109,22 @@ function createCallNorm(block: SubstrateBlock, call: any, args: any) {
   });
 }
 
-function createAddressMapping(mappedAddress: MappedAddress) {
+function createAddressMapping(mappedAddress: MappedAddress): AddressMapping {
   return new AddressMapping({
     id: mappedAddress.hex,
     ss58: mappedAddress.ss58,
+  });
+}
+
+export function addAddressesToMap(
+  itemName: string,
+  args: any,
+  addressArgs: AddressArgs,
+  addressMappings: Map<string, AddressMapping>
+): void {
+  const mappedAddresses = mapAddresses(itemName, args, addressArgs);
+  mappedAddresses.forEach((mappedAddress) => {
+    const addressMapping = createAddressMapping(mappedAddress);
+    addressMappings.set(mappedAddress.hex, addressMapping);
   });
 }
