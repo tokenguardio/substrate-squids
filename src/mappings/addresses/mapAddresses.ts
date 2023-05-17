62437c7a757627e01c36817ed5a667ac58ad4ab3
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-05-17 14:42:29 +0200

    refactor, add address mapping to all events/calls

---
+++ src/mappings/addresses/mapAddresses.ts
@@ -0,0 +1,21 @@
+import * as ss58 from "@subsquid/ss58";
+import { MappedAddress, AddressArgs } from "../../interfaces/mappings/specific";
+import { decodeHex } from "@subsquid/util-internal-hex";
+
+export function mapAddresses(
+  itemName: string,
+  args: any,
+  addressArgs: AddressArgs
+): MappedAddress[] {
+  const addressArgNames = addressArgs[itemName] || [];
+  const addresses: string[] = addressArgNames
+    .map((name) => args[name])
+    .filter(Boolean);
+
+  return addresses.map((address) => {
+    return {
+      hex: address,
+      ss58: ss58.codec("hydradx").encode(decodeHex(address)),
+    };
+  });
+}
