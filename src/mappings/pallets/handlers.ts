ee3fb06d517f6f7b40635bfceb6d861bfb319eb9
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-05-17 13:46:26 +0200

    refactor, add address mapping to all events/calls

---
+++ src/mappings/pallets/handlers.ts
@@ -0,0 +1,15 @@
+import { normalizeBalancesEventsArgs } from "./events/balances";
+import { normalizeContractsEventsArgs } from "./events/contracts";
+import { normalizeSystemEventsArgs } from "./events/system";
+
+import { normalizeContractsCallsArgs } from "./calls/contracts";
+
+export const eventNormalizationHandlers: { [key: string]: any } = {
+  Balances: normalizeBalancesEventsArgs,
+  System: normalizeSystemEventsArgs,
+  Contracts: normalizeContractsEventsArgs,
+};
+
+export const callNormalizationHandlers: { [key: string]: any } = {
+  Contracts: normalizeContractsCallsArgs,
+};
