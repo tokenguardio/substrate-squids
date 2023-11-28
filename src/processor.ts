1462dae242953e6645b2d48abbe111631e51dd26
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2023-11-28 08:24:55 +0100

    use archive only

--- src/processor.ts
+++ src/processor.ts
@@ -61,7 +61,8 @@ export const processor = new EvmBatchProcessor()
       rewardValue: true,
       rewardType: true,
     },
-  });
+  })
+  .useArchiveOnly(true);
 
 export type Fields = EvmBatchProcessorFields<typeof processor>;
 export type Block = BlockHeader<Fields>;
