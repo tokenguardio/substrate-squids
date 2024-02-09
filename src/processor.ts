79d78c0ed9226c80a7b6b246f037bc2bb57895fb
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-02-09 15:56:21 +0100

    normal processor

--- src/processor.ts
+++ src/processor.ts
@@ -25,7 +25,9 @@ export const processor = new EvmBatchProcessor()
       : undefined,
   })
   .setFinalityConfirmation(75)
-  .addTransaction({})
+  .addTransaction({
+    traces: true,
+  })
   .addLog({
     topic0: [erc20Abi.events.Transfer.topic],
   })
