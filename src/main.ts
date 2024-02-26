3102ad56c29bc54bfe27887cedca04b0c1f7404f
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-02-26 15:14:51 +0100

    state_schema as env

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ let currentTransactionId: string | null = null;
 let traceTree: TraceTree = new TraceTree("");
 
 processor.run(
-  new TypeormDatabase({ stateSchema: "evm_fungible_processor" }),
+  new TypeormDatabase({ stateSchema: process.env.STATE_SCHEMA }),
   async (ctx) => {
     const transactions: Transaction[] = [];
     const traceCreates: TraceCreate[] = [];
