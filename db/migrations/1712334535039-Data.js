03eb286d547bb9a5fa27fc60fe9bf852ba1356e2
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-04-05 18:29:36 +0200

    update WasmContractAbi table schema

--- db/migrations/1712334535039-Data.js
+++ db/migrations/1712334535039-Data.js
@@ -1,8 +1,8 @@
-module.exports = class Data1712332277937 {
-    name = 'Data1712332277937'
+module.exports = class Data1712334535039 {
+    name = 'Data1712334535039'
 
     async up(db) {
-        await db.query(`CREATE TABLE "wasm_contract_abi" ("id" character varying NOT NULL, "dapp_name" text NOT NULL, "abi" jsonb NOT NULL, CONSTRAINT "PK_4a938f3f82d0a85a20877451a4b" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE TABLE "wasm_contract_abi" ("id" character varying NOT NULL, "dapp_name" text NOT NULL, "abi" jsonb NOT NULL, "label" text, "from_block" numeric, "added_by" text, CONSTRAINT "PK_4a938f3f82d0a85a20877451a4b" PRIMARY KEY ("id"))`)
         await db.query(`CREATE TABLE "substrate_extrinsic" ("id" character varying NOT NULL, "block_number" integer NOT NULL, "extrinsic_hash" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "fee" numeric NOT NULL, "tip" numeric, "success" boolean NOT NULL, CONSTRAINT "PK_a4c7ce64007d5d29f412c071373" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_edea521b233747ee11ef03662c" ON "substrate_extrinsic" ("timestamp") `)
         await db.query(`CREATE TABLE "dapp_activity" ("id" character varying NOT NULL, "call_id" text NOT NULL, "extrinsic_hash" text NOT NULL, "block_number" integer NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "dapp_name" text NOT NULL, "caller" text, "contract" text NOT NULL, "success" boolean, "type" character varying(5) NOT NULL, "name" text NOT NULL, "value" numeric, "decoded_args" jsonb, "extrinsic_id" character varying, CONSTRAINT "PK_7fcef44cd298cda4cd2b2f5eca4" PRIMARY KEY ("id"))`)
--- src/model/generated/wasmContractAbi.model.ts
+++ src/model/generated/wasmContractAbi.model.ts
@@ -1,4 +1,5 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_} from "typeorm"
+import * as marshal from "./marshal"
 
 @Entity_()
 export class WasmContractAbi {
@@ -14,4 +15,13 @@ export class WasmContractAbi {
 
     @Column_("jsonb", {nullable: false})
     abi!: unknown
+
+    @Column_("text", {nullable: true})
+    label!: string | undefined | null
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
+    fromBlock!: bigint | undefined | null
+
+    @Column_("text", {nullable: true})
+    addedBy!: string | undefined | null
 }
