40b504476619e3953a88a324cfa7525e9a6b24f5
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-04-17 11:21:20 +0200

    assert envs

--- src/processing/fTokenCreation.ts
+++ src/processing/fTokenCreation.ts
@@ -1,5 +1,6 @@
 import { Store } from "@subsquid/typeorm-store";
 import { In } from "typeorm";
+import { assertNotNull } from "@subsquid/util-internal";
 import { ProcessorContext } from "../processor";
 import { FToken } from "../model";
 import { Multicall } from "../abi/multicall";
@@ -60,13 +61,12 @@ async function performMulticall(
   const multicall = new Multicall(
     ctx,
     lastBlock.header,
-    process.env.MULTICALL_CONTRACT ??
-      "0x6477204E12A7236b9619385ea453F370aD897bb2"
+    assertNotNull(process.env.MULTICALL_CONTRACT)
   );
   return await multicall.tryAggregate(
     contractFunction,
     addresses.map((address) => [address, [] as any[]] as [string, any[]]),
-    parseInt(process.env.MULTICALL_BATCH_SIZE ?? "100")
+    parseInt(assertNotNull(process.env.MULTICALL_BATCH_SIZE))
   );
 }
 
