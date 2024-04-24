585779efdaba20e97f09f835a570703b128bcf3b
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-04-24 17:30:32 +0200

    adjust to bsc

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,20 +12,6 @@ export function convertToTransactionType(type: number): EvmTransactionType {
       return EvmTransactionType.EIP1559;
     case 3:
       return EvmTransactionType.EIP4844;
-    case 100:
-      return EvmTransactionType.ARBITRUM_DEPOSIT;
-    case 101:
-      return EvmTransactionType.ARBITRUM_UNSIGNED;
-    case 102:
-      return EvmTransactionType.ARBITRUM_CONTRACT;
-    case 104:
-      return EvmTransactionType.ARBITRUM_RETRY;
-    case 105:
-      return EvmTransactionType.ARBITRUM_SUBMIT_RETRYABLE;
-    case 106:
-      return EvmTransactionType.ARBITRUM_INTERNAL;
-    case 120:
-      return EvmTransactionType.ARBITRUM_LEGACY;
     default:
       throw new Error(`Unknown transaction type: ${type}`);
   }
