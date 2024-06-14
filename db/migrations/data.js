2188915f0d939184559d7f83daff0f3b2d353908
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-06-14 18:08:40 +0200

    update migration

--- db/migrations/data.js
+++ db/migrations/data.js
@@ -3,11 +3,11 @@ const { assertNotNull } = require("@subsquid/substrate-processor");
 const dappId = assertNotNull(process.env.DAPP_ID);
 
 module.exports = class Data {
-  name = `Data${dappId}`;
+  name = `Data${Date.now()}`;
 
   async up(db) {
     await db.query(
-      `CREATE TABLE "dapp_analytics_${dappId}" ("id" character varying NOT NULL, "call_id" text NOT NULL, "extrinsic_hash" text NOT NULL, "block_number" integer NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "dapp_id" text NOT NULL, "caller" text, "contract" text NOT NULL, "success" boolean, "type" character varying(5) NOT NULL, "name" text NOT NULL, "value" numeric, "decoded_args" jsonb, CONSTRAINT "PK_8860ed8578a6f742739a0bea0ae" PRIMARY KEY ("id"))`
+      `CREATE TABLE "dapp_analytics_${dappId}" ("id" character varying NOT NULL, "call_id" text NOT NULL, "extrinsic_hash" text NOT NULL, "block_number" integer NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "dapp_id" text NOT NULL, "caller" text, "contract" text NOT NULL, "success" boolean, "type" character varying(5) NOT NULL, "name" text NOT NULL, "value" numeric, "decoded_args" jsonb, CONSTRAINT "PK_${dappId}" PRIMARY KEY ("id"))`
     );
   }
 
