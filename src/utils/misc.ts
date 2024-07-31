57d1f05df8cc5d83be58447451c6d1588eee6eec
Piotr Jozefowicz <piotr.jozefowicz@tokenguard.io>
2024-07-31 13:02:28 +0200

    change rpc settings

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -65,6 +65,40 @@ function clearNullBytes(rawStr: string): string {
   return rawStr ? rawStr.replace(/\0/g, "") : rawStr;
 }
 
+/**
+ * Utility function to get an environment variable value as a boolean.
+ * If the environment variable value does not exist, it returns the default value.
+ *
+ * @param {string | undefined} value - The value of the environment variable.
+ * @param {boolean} [defaultValue=false] - The default value to return if the environment variable value is not set.
+ * @returns {boolean} - The environment variable value as a boolean, or the default value.
+ */
 export function getEnvBoolean(value: undefined | string, defaultValue = false) {
   return value ? value.toLowerCase() === "true" : defaultValue;
+}
+
+/**
+ * Utility function to get an environment variable value as a number.
+ * If the environment variable value does not exist, it returns the default value or undefined.
+ * If the environment variable value is not a valid number, it throws an error.
+ *
+ * @param {string | undefined} envValue - The value of the environment variable.
+ * @param {number} [defaultValue] - The default value to return if the environment variable value is not set.
+ * @returns {number | undefined} - The environment variable value as a number, or the default value, or undefined.
+ * @throws {Error} - Throws an error if the environment variable value is not a valid number.
+ */
+export function getEnvNumber(
+  envValue: string | undefined | null,
+  defaultValue?: number
+): number | undefined {
+  if (envValue != undefined && envValue.trim() !== "") {
+    const numberValue = Number(envValue);
+    if (isNaN(numberValue)) {
+      throw new Error(
+        `Environment variable value "${envValue}" is not a valid number.`
+      );
+    }
+    return numberValue;
+  }
+  return defaultValue;
 }
